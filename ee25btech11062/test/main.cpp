b201e1a3656eac3eccc0ac40487a5334261191b9
Vivek-Kumar-1907 <vivekkkrumar2007@gmail.com>
2026-03-15 18:16:14 +0530

    some

--- ee25btech11062/test/main.cpp
+++ ee25btech11062/test/main.cpp
@@ -14,11 +14,11 @@ int main(void) {
     while (1) {
         // Set Pin 13 HIGH (PORTB5)
         PORTB |= (1 << PORTB5);
-        _delay_ms(1000);
+        _delay_ms(2000);
 
         // Set Pin 13 LOW
         PORTB &= ~(1 << PORTB5);
-        _delay_ms(2000);
+        _delay_ms(100);
     }
 
     return 0;
